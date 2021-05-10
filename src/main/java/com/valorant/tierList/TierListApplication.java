47ed1248873471923e59520b671773496984ccd0
will <willmiller629@gmail.com>
2021-05-10 14:10:59 -0400

    initial commit

---
+++ src/main/java/com/valorant/tierList/TierListApplication.java
@@ -0,0 +1,13 @@
+package com.valorant.tierList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valorant/tierList/TierListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valorant.tierList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TierListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
