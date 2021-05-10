0536c1a7f295eec56a97d94d5a9453189d596c09
will <willmiller629@gmail.com>
2021-05-10 14:24:25 -0400

    basic Agent repo created

---
+++ src/main/java/com/valorant/tierList/Repository/AgentRepo.java
@@ -0,0 +1,15 @@
+package com.valorant.tierList.Repository;
+
+import com.valorant.tierList.Entity.Agent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AgentRepo extends JpaRepository {
+    Agent findById(long id);
+    Agent findByName(String name);
+    List<Agent> findAllByName(String name);
+    List<Agent> findByRole(String role);
+}
