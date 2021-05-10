606dd1465b2b23577ea04fe8fe083c994692b104
will <willmiller629@gmail.com>
2021-05-10 15:24:50 -0400

    basic Agent service created

---
+++ src/main/java/com/valorant/tierList/Service/AgentService.java
@@ -0,0 +1,32 @@
+package com.valorant.tierList.Service;
+
+import com.valorant.tierList.Entity.Agent;
+import com.valorant.tierList.Repository.AgentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgentService {
+    @Autowired
+    private AgentRepo agentRepo;
+
+    public AgentService(AgentRepo agentRepo){
+        this.agentRepo = agentRepo;
+    }
+
+    public Agent readAgentById(long id){
+        return agentRepo.findById(id);
+    }
+
+    public void deleteAgentById(long id){
+        boolean agentExists = agentRepo.existsById(id);
+
+        if (!agentExists){
+            throw new IllegalStateException(
+                    "Agent with id " + id + " cannot be found in the database"
+            );
+        }
+
+        agentRepo.deleteById(id);
+    }
+}
