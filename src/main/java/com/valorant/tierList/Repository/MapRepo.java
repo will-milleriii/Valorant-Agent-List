28db301fc6acc5157ed4092f1c4be1770995d041
will <willmiller629@gmail.com>
2021-05-13 15:22:22 -0400

    Basic Map Repo created

--- src/main/java/com/valorant/tierList/Repository/MapRepo.java
+++ src/main/java/com/valorant/tierList/Repository/MapRepo.java
@@ -1,7 +1,15 @@
 package com.valorant.tierList.Repository;
 
+import com.valorant.tierList.Entity.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface MapRepo {
+public interface MapRepo extends JpaRepository<Map, Long> {
+
+    Map findById(long id);
+    Map findByName(String name);
+    List<Map> findAllMapsByName(String name);
 }
