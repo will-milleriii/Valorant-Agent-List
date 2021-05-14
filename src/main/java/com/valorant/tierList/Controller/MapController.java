3dc3fd0c62bdc2b8f4c6522f51b19ab4b30a58fc
will <willmiller629@gmail.com>
2021-05-14 19:24:44 -0400

    read map by id added to map controller

--- src/main/java/com/valorant/tierList/Controller/MapController.java
+++ src/main/java/com/valorant/tierList/Controller/MapController.java
@@ -4,10 +4,7 @@ import com.valorant.tierList.Entity.Map;
 import com.valorant.tierList.Service.MapService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/valorant/maps")
@@ -23,5 +20,13 @@ public class MapController {
     public ResponseEntity<Map> createMap(@RequestBody Map map){
         Map newMap = mapService.createMap(map);
         return new ResponseEntity<>(map, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map> readById(@PathVariable long id) throws Exception {
+        if (new ResponseEntity<>(mapService.readMapById(id), HttpStatus.OK) == null)throw new Exception("Sorry! Cannot find that Map by the given ID");
+        else{
+            return new ResponseEntity<>(mapService.readMapById(id), HttpStatus.OK);
+        }
     }
 }
